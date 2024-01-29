a373bf81d6e40cb6f57a40a1f13e373abc006c06
RistoFlink <kenssi@gmail.com>
2024-01-29 14:55:16 +0200

    Update the Movie component to display more information about the movie

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,8 +1,27 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 const Movie = () => {
+    const [movie, setMovie] = useState({});
+    let { id } = useParams();
+
+    useEffect(() => {
+        let myMovie = {
+            id: 1,
+            title: "House",
+            release_date: "1977-07-30",
+            runtime: 88,
+            mpaa_rating: "R",
+            description: "Enter later"
+        }
+        setMovie(myMovie);
+    }, [id])
     return (
         <div>
-            <h2>Movie</h2>
+            <h2>Movie: {movie.title}</h2>
+            <small><em>{movie.release_date}, {movie.runtime} minutes, Rated: {movie.mpaa_rating}</em></small>
             <hr />
+            <p>{movie.description}</p>
         </div>
     )
 }
